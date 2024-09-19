57041bae430f8afaa0c00d64b2e9accaaa52b021
najia <najiaknizi199@gmail.com>
2024-09-19 12:23:32 +0100

    gestion projet

--- src/app/projet/ajouter-projet/ajouter-projet.component.ts
+++ src/app/projet/ajouter-projet/ajouter-projet.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { ProjetServiceService } from 'src/app/Services/projet-service.service';
+import { Projets } from 'src/Projet';
 
 @Component({
   selector: 'app-ajouter-projet',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./ajouter-projet.component.css']
 })
 export class AjouterProjetComponent {
+  nouveauProjet: Projets = {
+    nom_projet: '',
+    description_projet: '',
+    date_debut: new Date(),
+    date_fin: new Date(),
+    budget: 0,
+    id: 0
+  };
+constructor(private projetServiceService: ProjetServiceService,
+  private router : Router,
+) {}
 
+  ajouterProjet() {
+    this.projetServiceService.ajouterProjet(this.nouveauProjet).subscribe(
+      response => {
+        console.log('Projet ajouté avec succès', response);
+        this.router.navigateByUrl("afficher-projet");
+      },
+      error => {
+        console.error('Erreur lors de l\'ajout du projet', error);
+      }
+    );
+  }
 }
